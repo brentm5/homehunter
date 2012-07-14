da60f73ed0feb3712f26d213006625404770975b
Brent Montague <bigbam505@gmail.com>
2012-07-14 17:16:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homehunter::Application.config.secret_token = '6f0835af2ea0ee61f050ac04c24c39f19191d17e56d7c531ac4a368ded97c92688f0006a3ec7187c2f2d23faf671d0a2f5bbbc442c921dad00d7593353e02a4b'
