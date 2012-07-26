687cd10dc005c824f3c89496e02407d82e44d781
Brent Montague <bigbam505@gmail.com>
2012-07-25 22:24:43 -0400

    Removed factory girl and added verification of description save

--- features/step_definitions/hunt_steps.rb
+++ features/step_definitions/hunt_steps.rb
@@ -1,16 +1,15 @@
 Then /^I should be able to create a hunt$/ do
   visit(new_hunt_url)
-  hunt = build(:hunt)
-  fill_in 'hunt_name', :with => hunt.name
-  fill_in 'hunt_description', :with => hunt.description
+  fill_in 'hunt_name', :with => 'Hunt Name'
+  fill_in 'hunt_description', :with => 'Description of a hunt'
   click_button 'Start Hunt'
-  page.should have_content hunt.name
+  page.should have_content 'Hunt Name'
+  page.should have_content 'Description of a hunt'
 end
 
 Then /^I should not be able to create an invalid hunt$/ do
   visit(new_hunt_url)
-  hunt = build(:hunt)
-  fill_in 'hunt_name', :with => hunt.name
+  fill_in 'hunt_name', :with => 'Hunt Name'
   click_button 'Start Hunt'
   page.should have_content 'Start a Hunt'
 end
