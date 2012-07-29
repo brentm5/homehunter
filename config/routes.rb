d114c5246e06b390125502604c95a6f5bafecbde
Brent Montague <bigbam505@gmail.com>
2012-07-29 19:48:38 -0400

    Added redirect to new hunt after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Homehunter::Application.routes.draw do
   root :to => 'home#index'
-  devise_for :users
+  devise_for :users , :controllers => { :registrations => "registrations" }
 
   resources :hunts, :only => [:new, :create, :show]
 end
